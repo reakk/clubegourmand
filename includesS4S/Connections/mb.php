287ede619731bd9ff7a79ce1487528249085128e
reakk <github@otaviomarcal.com.br>
2015-02-19 15:40:35 -0200

    del

--- includesS4S/Connections/mb.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$hostname_mb = "189.126.99.226";
-	$database_mb = "portal_gourmand";
-	$username_mb = "portal_gourmand";
-	$password_mb = "BlRj@15";
-	$mb = mysql_pconnect($hostname_mb, $username_mb, $password_mb) or trigger_error(mysql_error(),E_USER_ERROR); 
-?>
